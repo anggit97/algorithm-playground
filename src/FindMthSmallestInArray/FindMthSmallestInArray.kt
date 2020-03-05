13eac1f1da06af78bbb01be36d3267b92b316a77
anggitprayogo <anggitprayogo@gmail.com>
2020-03-05 19:06:05 +0700

    Add Find Mth Smallest Number In Array

---
+++ src/FindMthSmallestInArray/FindMthSmallestInArray.kt
@@ -0,0 +1,33 @@
+package FindMthSmallestInArray
+
+fun main() {
+    val testCaseArray = mutableListOf(
+        intArrayOf(1, 3),
+        intArrayOf(2, 4, 6),
+        intArrayOf(0, 9, 10, 11)
+    )
+
+    val testCaseArray2 = mutableListOf(
+        intArrayOf(1, 3, 20),
+        intArrayOf(2, 4, 6)
+    )
+
+    val m = 5
+
+    println("Find the smallest $m-th in array : ")
+    println(findMthSmallestValueSmallestKShortestArray(testCaseArray2, m))
+}
+
+fun findMthSmallestValueSmallestKShortestArray(
+    testCaseArray: MutableList<IntArray>,
+    m: Int
+): Int {
+    val result = mutableListOf<Int>()
+    for (arr in testCaseArray) {
+        arr.forEach {
+            result.add(it)
+        }
+    }
+    val sortedArray = result.sorted()
+    return sortedArray[m-1]
+}
