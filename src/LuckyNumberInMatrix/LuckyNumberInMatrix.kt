4d96add0f7103f4dfc16fa9457e37c03e66a8e2f
anggitprayogo <anggitprayogo@gmail.com>
2020-03-24 07:16:04 +0700

    Add Valid Solution Lucky Number In MAtrix

--- src/LuckyNumberInMatrix/LuckyNumberInMatrix.kt
+++ src/LuckyNumberInMatrix/LuckyNumberInMatrix.kt
@@ -1,5 +1,8 @@
 package LuckyNumberInMatrix
 
+import kotlin.math.max
+import kotlin.math.min
+
 class LuckyNumberInMatrix {
 
     fun luckyNumbers(matrix: Array<IntArray>): List<Int> {
@@ -20,5 +23,33 @@ class LuckyNumberInMatrix {
         }
 
         return listOf(matrix[maxNumberIndexColumn][0])
+    }
+
+    fun luckyNumberFindAllPossible(matrix: Array<IntArray>): List<Int> {
+        val m = matrix.size
+        val n = matrix[0].size
+        val mi = IntArray(m)
+        val mx = IntArray(n)
+
+        var k = 0
+        repeat(mi.count()) {
+            mi[k] = Int.MAX_VALUE
+            k++
+        }
+
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                mi[i] = min(matrix[i][j], mi[i])
+                mx[j] = max(matrix[i][j], mx[j])
+            }
+        }
+
+        val res = mutableListOf<Int>()
+        for (i in 0 until m)
+            for (j in 0 until n)
+                if (mi[i] == mx[j])
+                    res.add(mi[i])
+
+        return res
     }
 }
--- src/LuckyNumberInMatrix/LuckyNumberInMatrixTest.kt
+++ src/LuckyNumberInMatrix/LuckyNumberInMatrixTest.kt
@@ -47,4 +47,40 @@ class LuckyNumberInMatrixTest {
             )
         )
     }
+
+
+    @Test
+    fun luckyNumberInMatric2() {
+        assertEquals(
+            listOf(15),
+            SUT.luckyNumberFindAllPossible(
+                arrayOf(
+                    intArrayOf(3, 7, 8),
+                    intArrayOf(9, 11, 13),
+                    intArrayOf(15, 16, 17)
+                )
+            )
+        )
+
+        assertEquals(
+            listOf(12),
+            SUT.luckyNumberFindAllPossible(
+                arrayOf(
+                    intArrayOf(1, 10, 4, 2),
+                    intArrayOf(9, 3, 8, 7),
+                    intArrayOf(15, 16, 17, 12)
+                )
+            )
+        )
+
+        assertEquals(
+            listOf(7),
+            SUT.luckyNumberFindAllPossible(
+                arrayOf(
+                    intArrayOf(7, 8),
+                    intArrayOf(1, 2)
+                )
+            )
+        )
+    }
 }
