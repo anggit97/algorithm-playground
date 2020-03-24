562ccedeee017afa89e8273c5687682b6ba1a512
anggitprayogo <anggitprayogo@gmail.com>
2020-03-24 07:02:02 +0700

    Add Passed Test Solution Lucky Number In Matrix

--- src/LuckyNumberInMatrix/LuckyNumberInMatrix.kt
+++ src/LuckyNumberInMatrix/LuckyNumberInMatrix.kt
@@ -8,7 +8,17 @@ class LuckyNumberInMatrix {
             matrix[index] = matrix[index].sortedArray()
         }
 
+        var maxNumber = matrix[0][0]
+        var maxNumberIndexColumn = 0
 
-        return listOf(matrix[matrix.size - 1][0])
+        //Get All Possible Maximum Element in Column 0
+        for ((index, item) in matrix.withIndex()) {
+            if (maxNumber < item[0]) {
+                maxNumber = item[0]
+                maxNumberIndexColumn = index
+            }
+        }
+
+        return listOf(matrix[maxNumberIndexColumn][0])
     }
 }
