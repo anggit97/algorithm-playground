a6e783a5bf6506f5070db443e299dfd0717f4337
anggitprayogo <anggitprayogo@gmail.com>
2020-03-28 06:30:02 +0700

    Add valid solution (pass all test) Save The Prisoner Problem

--- src/SaveThePrisoner/SaveThePrisoner.kt
+++ src/SaveThePrisoner/SaveThePrisoner.kt
@@ -4,12 +4,32 @@ class SaveThePrisoner {
 
     fun saveThePrisoner(n: Int, m: Int, s: Int): Int {
         var treat = m
-        var result = s-1
-        while (treat > 0){
-            if (result > n) result = s-1
+        var result = s - 1
+        while (treat > 0) {
+            if (result > n) result = s - 1
             result += 1
             treat--
         }
         return result
+    }
+
+    fun saveThePrisonerV2(n: Int, m: Int, s: Int): Int {
+        val result: Int
+        val diffNumber1 = s - 1
+        val calc = m.rem(n)
+        result = calc + diffNumber1
+        return result
+    }
+
+    fun saveThePrisonerV3(n: Int, m: Int, s: Int): Int {
+        val a = s + m - 1
+        if (a > n) {
+            if (a % n == 0) {
+                return n
+            }
+            return a % n
+        }
+
+        return a
     }
 }
--- src/SaveThePrisoner/SaveThePrisonerTest.kt
+++ src/SaveThePrisoner/SaveThePrisonerTest.kt
@@ -20,4 +20,20 @@ class SaveThePrisonerTest {
         assertEquals(6, SUT.saveThePrisoner(7, 19, 2))
         assertEquals(3, SUT.saveThePrisoner(3 ,7, 3))
     }
+
+    @Test
+    fun saveThePrisonnerV2() {
+        assertEquals(2, SUT.saveThePrisonerV2(5, 2, 1))
+        assertEquals(3, SUT.saveThePrisonerV2(5, 2, 2))
+        assertEquals(6, SUT.saveThePrisonerV2(7, 19, 2))
+        assertEquals(3, SUT.saveThePrisonerV2(3 ,7, 3))
+    }
+
+    @Test
+    fun saveThePrisonnerV3() {
+        assertEquals(2, SUT.saveThePrisonerV3(5, 2, 1))
+        assertEquals(3, SUT.saveThePrisonerV3(5, 2, 2))
+        assertEquals(6, SUT.saveThePrisonerV3(7, 19, 2))
+        assertEquals(3, SUT.saveThePrisonerV3(3 ,7, 3))
+    }
 }
