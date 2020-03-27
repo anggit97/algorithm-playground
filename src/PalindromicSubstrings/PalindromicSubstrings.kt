7bbbc95b7a4a05da1227a4817d9126a87f87d808
anggitprayogo <anggitprayogo@gmail.com>
2020-03-27 20:36:51 +0700

    Add Palindrom Substring solution

--- src/PalindromicSubstrings/PalindromicSubstrings.kt
+++ src/PalindromicSubstrings/PalindromicSubstrings.kt
@@ -1,8 +1,33 @@
 package PalindromicSubstrings
 
+import ValidParentheses.isValidParentheses2
+
 class PalindromicSubstrings {
 
     fun countSubstrings(s: String): Int {
-        return 0
+        val mutableList = mutableListOf<String>()
+        var idx = 0
+
+        for (item in s) {
+            var start = ""
+            for (index2 in idx until s.length) {
+                start = start.plus(s[index2])
+                mutableList.add(start)
+            }
+            idx++
+        }
+
+        var countPalindrom = 0
+        mutableList.forEach {
+            if (isPalindromeWithFunction(it)) countPalindrom++
+        }
+
+        return countPalindrom
+    }
+
+    fun isPalindromeWithFunction(s: String): Boolean {
+        val reverse = s.reversed().toCharArray()
+        val notRevers = s.toCharArray()
+        return notRevers.contentEquals(reverse)
     }
 }
