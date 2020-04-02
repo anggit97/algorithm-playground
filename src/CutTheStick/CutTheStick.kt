31c813f079df86e91c30ac6a1b0c2733c71335fc
anggitprayogo <anggitprayogo@gmail.com>
2020-04-02 20:29:48 +0700

    Add Cut The Stick solution (just one test case)

--- src/CutTheStick/CutTheStick.kt
+++ src/CutTheStick/CutTheStick.kt
@@ -3,6 +3,26 @@ package CutTheStick
 class CutTheStick {
 
     fun cutTheSticks(arr: Array<Int>): Array<Int> {
-        return arr
+        val result = mutableListOf<Int>()
+        var arrTemp = arr.toMutableList()
+
+        result.add(arrTemp.count())
+
+        while (arrTemp.isNotEmpty()) {
+
+            val minVal = arrTemp.min()
+
+            for ((index, item) in arrTemp.withIndex()) {
+                arrTemp[index] = item - minVal!!
+            }
+
+            val x = arrTemp.filter { it > 0 }
+
+            arrTemp = x.toMutableList()
+
+            result.add(arrTemp.count())
+        }
+
+        return result.filter { it > 0 }.toTypedArray()
     }
 }
