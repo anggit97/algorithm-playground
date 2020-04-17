39452f4e49ecc50f1a8339d079d2bff2bb6d52ed
anggitprayogo <anggitprayogo@gmail.com>
2020-04-17 18:53:04 +0700

    Add more test in backward read primes problem

--- src/BackwardReadPrimes/BackwardReadPrimesTest.kt
+++ src/BackwardReadPrimes/BackwardReadPrimesTest.kt
@@ -17,6 +17,7 @@ class BackwardReadPrimesTest{
     fun backwardReadPrimes(){
         assertEquals("13 17 31 37 71 73 79 97", SUT.backwardsPrime(2, 100))
         assertEquals("7027 7043 7057", SUT.backwardsPrime(7000, 7100))
+        assertEquals("70001 70009 70061 70079 70121 70141 70163 70241", SUT.backwardsPrime(70000, 70245))
     }
 
     @Test
