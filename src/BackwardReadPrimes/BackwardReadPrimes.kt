59213102504c57572a5c58305910fcb2814ecf34
anggitprayogo <anggitprayogo@gmail.com>
2020-04-17 18:50:20 +0700

    Add solution for backward read primes

--- src/BackwardReadPrimes/BackwardReadPrimes.kt
+++ src/BackwardReadPrimes/BackwardReadPrimes.kt
@@ -2,7 +2,30 @@ package BackwardReadPrimes
 
 class BackwardReadPrimes {
 
-    fun backwardsPrime(start:Long, end:Long):String {
-        return ""
+    fun backwardsPrime(start: Long, end: Long): String {
+        var result = ""
+
+        for (i in start..end) {
+            val reversePrimeNumber = i.toString().reversed().toLong()
+
+            if (i.toString().length == 1 || reversePrimeNumber.toString().length == 1) continue
+
+            if (i == reversePrimeNumber) continue
+
+            if (!isPrimeNumber(i) || !isPrimeNumber(reversePrimeNumber)) continue
+
+            result += " $i"
+        }
+
+        return result.trim()
+    }
+
+    fun isPrimeNumber(number: Long): Boolean {
+        for (i in 2..number / 2) {
+            if (number % i == 0L) {
+                return false
+            }
+        }
+        return true
     }
 }
--- src/BackwardReadPrimes/BackwardReadPrimesTest.kt
+++ src/BackwardReadPrimes/BackwardReadPrimesTest.kt
@@ -15,6 +15,12 @@ class BackwardReadPrimesTest{
 
     @Test
     fun backwardReadPrimes(){
+        assertEquals("13 17 31 37 71 73 79 97", SUT.backwardsPrime(2, 100))
         assertEquals("7027 7043 7057", SUT.backwardsPrime(7000, 7100))
+    }
+
+    @Test
+    fun isPrivateNumber(){
+        assertEquals(true, SUT.isPrimeNumber(29))
     }
 }
