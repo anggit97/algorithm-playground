81852b543d498e7e551cd7b480b1d94ed883cee8
anggitprayogo <anggitprayogo@gmail.com>
2020-04-19 16:29:48 +0700

    Add solution between to set

--- src/BetweenTwoNumbers/BetweenTwoNumbers.kt
+++ src/BetweenTwoNumbers/BetweenTwoNumbers.kt
@@ -3,7 +3,44 @@ package BetweenTwoNumbers
 class BetweenTwoNumbers {
 
     fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
+        val lcp = mutableListOf<Int>()
+        val result = mutableListOf<Int>()
+        val maxA = a.max()!!
+        val minB = b.min()!!
 
-        return 0
+
+        //gcp
+        for (num in maxA..minB) {
+            var flag = true
+
+            for (numA in a) {
+                if (num.rem(numA) != 0) {
+                    flag = false
+                    break
+                }
+            }
+
+            if (flag) {
+                lcp.add(num)
+            }
+        }
+
+
+        //lcp
+        for (num in lcp){
+            var flag = true
+            for (numB in b){
+                if (numB.rem(num) != 0){
+                    flag = false
+                    break
+                }
+            }
+
+            if (flag){
+                result.add(num)
+            }
+        }
+
+        return result.size
     }
 }
