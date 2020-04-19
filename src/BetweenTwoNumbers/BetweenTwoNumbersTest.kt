c307b5632f6b9c4945de06c9f12f617aebd3b218
anggitprayogo <anggitprayogo@gmail.com>
2020-04-19 16:30:41 +0700

    Add more test in between two set

--- src/BetweenTwoNumbers/BetweenTwoNumbersTest.kt
+++ src/BetweenTwoNumbers/BetweenTwoNumbersTest.kt
@@ -16,5 +16,6 @@ class BetweenTwoNumbersTest {
     @Test
     fun betweenTwoNumbers() {
         assertEquals(2, SUT.getTotalX(arrayOf(2, 6), arrayOf(24, 36)))
+        assertEquals(3, SUT.getTotalX(arrayOf(2, 4), arrayOf(16, 32, 96)))
     }
 }
