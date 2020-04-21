78c35f8972a82189af5baf5bec0cba909fd16d2e
anggitprayogo <anggitprayogo@gmail.com>
2020-04-21 07:26:15 +0700

    optimized solution of sherlock and squares problem

--- src/SherlockAndSquares/SherlockAndSquares.kt
+++ src/SherlockAndSquares/SherlockAndSquares.kt
@@ -5,11 +5,20 @@ import kotlin.math.sqrt
 class SherlockAndSquares {
 
     fun squares(a: Int, b: Int): Int {
-        var count = 0
-        for (i in a..b){
-            val number = sqrt(i.toDouble())
-            if (number%1.0 == 0.0) count++
+//        var count = 0
+//        for (i in a..b){
+//            val number = sqrt(i.toDouble())
+//            if (number%1.0 == 0.0) count++
+//        }
+//        return count
+
+        var numOfSquares = 0
+        var x = 1
+        while (x*x < a) x++
+        while (x*x <= b){
+            numOfSquares++
+            x++
         }
-        return count
+        return numOfSquares
     }
 }
